b342e774412b44be153c816741124669217abacb
Sudeepa-R <119294212+Sudeepa-R@users.noreply.github.com>
2024-02-08 20:53:23 +0530

    Update topbar.php

--- topbar.php
+++ topbar.php
@@ -9,7 +9,7 @@
   		
   		</div>
       <div class="col-md-4 float-left text-white">
-        <large><b>Recruitment Management System</b></large>
+        <large><b>PayRoll Management System</b></large>
       </div>
 	  	<div class="col-md-2 float-right text-white">
 	  		<a href="ajax.php?action=logout" class="text-white"><?php echo $_SESSION['login_name'] ?> <i class="fa fa-power-off"></i></a>
